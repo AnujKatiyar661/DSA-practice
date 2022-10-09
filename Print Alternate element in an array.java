9614f1ff6ab9b45457a79ec0c4eca5f5d0ec31b2
anuj katiyar <106858176+AnujKatiyar661@users.noreply.github.com>
2022-10-10 03:35:26 +0530

    Create Print Alternate element in an array.java

---
+++ Print Alternate element in an array.java
@@ -0,0 +1,14 @@
+
+// ar[] is the array 
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    
+    // code here
+    for(int i=0;i<n;i=i+2)
+    {
+        printf("%d ",ar[i]);
+    }
+    
+    
+}
